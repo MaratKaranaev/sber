f245c9c96bf6801f657d8c0be30a211e8299dc0b
MaratKaranaev <karanaevmar@yandex.ru>
2024-04-17 16:23:39 +0300

    fq

--- main.py
+++ main.py
@@ -15,3 +15,13 @@ num1 = 5
 num2 = 3
 result = multiply_numbers(num1, num2)
 print(f"Произведение чисел {num1} и {num2} равно {result}")
+
+
+def repeat_string (string, times) :
+    return string * times
+
+text = "Hello World"
+repetitions = 3
+result = repeat_string(text, repetitions)
+print(f"Результат повторения строки"
+'{text}' {repetitions} раз {result}")
