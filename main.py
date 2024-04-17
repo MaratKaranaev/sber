683e78d27101bef4197edacdd7a599bd12f4797c
MaratKaranaev <karanaevmar@yandex.ru>
2024-04-17 16:11:33 +0300

    fq

---
+++ main.py
@@ -0,0 +1,9 @@
+def sum_numbers(a, b):
+    return a + b
+
+# Пример использования функции
+num1 = 5
+num2 = 3
+result = sum_numbers(num1, num2)
+print(f"Сумма чисел {num1} и {num2} равна {result}")
+
