a20184d26e98f59b226c6dadc3895b494d965453
MaratKaranaev <karanaevmar@yandex.ru>
2024-04-17 16:27:41 +0300

    fq

--- main.py
+++ main.py
@@ -6,13 +6,3 @@ num1 = 5
 num2 = 3
 result = sum_numbers(num1, num2)
 print(f"Сумма чисел {num1} и {num2} равна {result}")
-
-def multiply_numbers(a, b):
-    return a * b
-
-# Пример использования функции
-num1 = 5
-num2 = 3
-result = multiply_numbers(num1, num2)
-print(f"Произведение чисел {num1} и {num2} равно {result}")
-ew
